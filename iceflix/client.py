22e5dbe6394be1ba3680f2819cdbeb0be0b09bd5
JuanbaCGC <JuanBautista.Castano@alu.uclm.es>
2022-12-01 18:36:38 +0100

    getAuthenticator

--- iceflix/client.py
+++ iceflix/client.py
@@ -2,6 +2,7 @@
 # -- coding: utf-8 --
 
 import cmd
+import hashlib
 import sys
 import getpass
 import time
@@ -14,23 +15,28 @@ class ClientShell(cmd.Cmd):
         prompt: str = '(Client)'
         # ----- Opciones del menú -----
         def do_adminLogin(self, arg):
-            print("Introduce tu nombre de usuario:")
-            user_name = input()
-            print("Introduce ahora la contraseña:")
-            password = getpass.getpass("Contraseña: ")
+            print("Introduce el token:")
+            token = input()
 
         def do_userLogin(self,arg):
             print("Introduce tu nombre de usuario:")
             user_name = input()
             print("Introduce ahora la contraseña:")
             password = getpass.getpass("Contraseña: ")
-
+            pass_hash = hashlib.sha256(password.encode()).hexdigest()
+            print(pass_hash)
+            self.authenticator.refreshAuthorization(user_name, pass_hash)
         # def do_nameSearch(self,arg):
         #     print("Introduce el nombre por búsqueda...")
 
         def do_logout(self,arg):
             print("Vas a salir de tu usuario...")
             return True
+
+        def __init__(self, main):
+            super(ClientShell, self).__init__()
+            self.main = main
+            self.authenticator = main.getAuthenticator()
 
 class Client(Ice.Application):
     # ----- Clase Cliente -----
@@ -43,21 +49,22 @@ class Client(Ice.Application):
 
         counter = 0
         checked = False
+        main=""
         proxy = self.communicator().stringToProxy(argv[1])
-        while(counter != 3 or checked == False):
+        while(counter != 3 or checked):
             counter+=1
             try:
                 main = IceFlix.MainPrx.checkedCast(proxy)
-            except IceFlix.Exception:
-                print("LSJAFLSDJFSL")
-                continue
-            if not main:
-                print("Intento número ",counter," de conexión fallido por el proxy.")
-                time.sleep(5)
-            else:
                 checked = True
+            except:
+                print("Intento número ",counter," de conexión fallido por el proxy.")
+                time.sleep(1)
+                continue
 
-
-        ClientShell().cmdloop()
+        # if(checked):
+        ClientShell(main).cmdloop()
+        # else:
+        print("Saliendo del programa del cliente...")
+        return -1
 
 sys.exit(Client().main(sys.argv))
