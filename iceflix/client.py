26657b0e635f3c5cb8795b3f9b81dcc1d4259697
JuanbaCGC <JuanBautista.Castano@alu.uclm.es>
2022-12-01 17:02:20 +0100

    last changes

--- iceflix/client.py
+++ iceflix/client.py
@@ -4,8 +4,10 @@
 import cmd
 import sys
 import getpass
+import time
 import Ice
 Ice.loadSlice('iceflix.ice')
+import IceFlix
 
 class ClientShell(cmd.Cmd):
         intro = 'Bienvenido al IceFlix menu. Escribe "help" ó "?" para listar las opciones. \n'
@@ -16,38 +18,45 @@ class ClientShell(cmd.Cmd):
             user_name = input()
             print("Introduce ahora la contraseña:")
             password = getpass.getpass("Contraseña: ")
-        
+
         def do_userLogin(self,arg):
             print("Introduce tu nombre de usuario:")
             user_name = input()
             print("Introduce ahora la contraseña:")
             password = getpass.getpass("Contraseña: ")
-        
-        def do_nameSearch(self,arg):
-            print("Vas a buscar por nombre...")
-    
+
+        # def do_nameSearch(self,arg):
+        #     print("Introduce el nombre por búsqueda...")
+
         def do_logout(self,arg):
             print("Vas a salir de tu usuario...")
             return True
 
 class Client(Ice.Application):
     # ----- Clase Cliente -----
-        
+
     def run(self, argv):
         print("Cliente iniciado:")
         if(len(sys.argv) != 2):
             print("Tienes que insertar el proxy del main. \nSaliendo del programa...")
             return -1
-        
+
         counter = 0
-        if(counter != 3):
+        checked = False
+        proxy = self.communicator().stringToProxy(argv[1])
+        while(counter != 3 or checked == False):
             counter+=1
-            proxy = self.communicator().stringToProxy(argv[1])
+            # try:
             main = IceFlix.MainPrx.checkedCast(proxy)
-            
+            # except IceFlix.TemporaryUnavailable:
+
             if not main:
-                raise TemporaryUnavailable('Invalid proxy: ', proxy)
-            
+                print("Intento número ",counter," de conexión fallido por el proxy.")
+                time.sleep(5)
+            else:
+                checked = True
+
+
         ClientShell().cmdloop()
-        
+
 sys.exit(Client().main(sys.argv))
