f448973f65bf23b579a588d94f679c7a3aa052a3
JuanbaCGC <JuanBautista.Castano@alu.uclm.es>
2022-12-02 13:10:02 +0100

    User menu implementation

--- iceflix/client.py
+++ iceflix/client.py
@@ -14,30 +14,33 @@ import IceFlix
 class ClientShell(cmd.Cmd):
         intro = 'Bienvenido al IceFlix menu. Escribe "help" ó "?" para listar las opciones. \n'
         prompt: str = '(Client menu)'
-        # ----- Opciones del menú -----
+        # ----- Opciones del menú del cliente ----- #
         def do_adminLogin(self, arg):
             print("Introduce el token:")
             token = input()
             is_admin = self.authenticator.isAdmin(token)
-            is_admin = True
+            # is_admin = True
             if (is_admin):
                 print("Es admin... \n")
-                t = Thread(target=AdminShell(self.main).cmdloop())
+                t = Thread(target=AdminShell(self.main, token).cmdloop())
                 t.start()
                 t.join()
-                
 
         def do_userLogin(self,arg):
             print("Introduce tu nombre de usuario:")
             user_name = input()
-            print("Introduce ahora la contraseña:")
+            print("Introduce la contraseña:")
             password = getpass.getpass("Contraseña: ")
             pass_hash = hashlib.sha256(password.encode()).hexdigest()
-            print(pass_hash)
-            self.authenticator.refreshAuthorization(user_name, pass_hash)
+            user_token = self.authenticator.refreshAuthorization(user_name, pass_hash)
+            t = Thread(target=NormalUserShell(self.main, user_token).cmdloop())
+            t.start()
+            t.join()
 
+        def do_exit(self,arg):
+            print("Saliendo del cliente...")
+            return 1
         
-
         def __init__(self, main):
             super(ClientShell, self).__init__()
             self.main = main
@@ -46,12 +49,19 @@ class ClientShell(cmd.Cmd):
 class AdminShell(cmd.Cmd):
     intro = 'Menu de administrador. Escribe "help" ó "?" para listar las opciones. \n'
     prompt: str = '(Admin menu)'
-        
+    # ----- Opciones del menú del administrador ----- #
     def do_addUser(self,arg):
-        print("Adding a user")
+        print("Introduce el nombre del usuario a añadir:")
+        user_name = input()
+        print("Introduce la contraseña:")
+        password = getpass.getpass("Contraseña: ")
+        hassed_pash = hashlib.sha256(password.encode()).hexdigest()
+        self.authenticator.addUser(user_name, hassed_pash, self.admin_token)
         
-    def do_deleteUser(self,arg):
-        print("Deleting a user")
+    def do_removeUser(self,arg):
+        print("Introduce el nombre del usuario a eliminar:")
+        user_name = input()
+        self.authenticator.removeUser(user_name,self.admin_token)
         
     def do_renameFile(self,arg):
         print("Renaming a file")
@@ -63,17 +73,37 @@ class AdminShell(cmd.Cmd):
         print("Downloading a file")
         
     def do_logout(self,arg):
-        print("Loging out...")
+        print("Cerrando sesión del administrador...")
         return 1
         
-    def __init__(self, main):
-            super(AdminShell, self).__init__()
-            self.main = main
-            self.authenticator = main.getAuthenticator()
+    def __init__(self, main, admin_token):
+        super(AdminShell, self).__init__()
+        self.main = main
+        self.authenticator = main.getAuthenticator()
+        self.admin_token = admin_token
 
+class NormalUserShell(cmd.Cmd):
+    intro = 'Inicio de sesión completado. \nEscribe "help" ó "?" para listar las opciones. \n'
+    prompt: str = '(User)'
+    
+    def do_TilesByName(self,arg):
+        print("Buscando archivos por nombre...")
+        
+    def do_TilesByTag(self,arg):
+        print("Buscando archivos por tag...")
+        
+    def do_logout(self,arg):
+        print("Cerrando sesión del usuario")
+        return 1
+    
+    def __init__(self, main, user_token):
+        super(NormalUserShell, self).__init__()
+        self.main = main
+        self.authenticator = main.getAuthenticator()
+        self.user_token = user_token
+        
 class Client(Ice.Application):
     # ----- Clase Cliente -----
-
     def run(self, argv):
         print("Cliente iniciado:")
         if(len(sys.argv) != 2):
@@ -82,7 +112,7 @@ class Client(Ice.Application):
 
         counter = 0
         checked = False
-        main=""
+        #main=""
         proxy = self.communicator().stringToProxy(argv[1])
         while(counter != 3 or checked):
             counter+=1
@@ -90,14 +120,13 @@ class Client(Ice.Application):
                 main = IceFlix.MainPrx.checkedCast(proxy)
                 checked = True
             except:
-                print("Intento número ",counter," de conexión fallido por el proxy.")
-                time.sleep(1)
-                continue
+                print("Intento número",counter,"de conexión fallido por el proxy.")
+                time.sleep(5)
 
         if(checked):
             ClientShell(main).cmdloop()
         else:
-            print("Saliendo del programa del cliente...")
-        return -1
+            print("Se ha alcanzado el máximo de intentos de conexión. \nSaliendo del programa del cliente...")
+            return -1
 
 sys.exit(Client().main(sys.argv))
